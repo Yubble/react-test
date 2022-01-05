6e606c6cd898bd4f4b40eb2783188d02ed39d217
liuyanbao <liuyanbao@shuidi-inc.com>
2022-01-05 14:07:09 +0800

    完成jsx的认识学习

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -4,17 +4,21 @@
  * @Author: 刘燕保
  * @Date: 2022-01-04 21:10:33
  */
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import robots from './mockData/robot.json'
 import Robot from './components/Robot'
 
+const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />"
+const jsHacked = "javascript: alert('Hacked');"
 function App() {
   return (
-    <ul>
-      {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
-    </ul>
+    // react-jsx的规则，需要加<></>进行包裹
+    <>
+      <a href={jsHacked}>123</a>
+      <ul>
+        {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
+      </ul>
+    </>
   );
 }
 
