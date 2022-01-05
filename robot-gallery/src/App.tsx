37038292776f802f6f4b6935a53baf224fe85244
liuyanbao <liuyanbao@shuidi-inc.com>
2022-01-05 21:46:57 +0800

    feat: 完成字体与图片加载

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -9,13 +9,18 @@ import robots from './mockData/robot.json'
 import Robot from './components/Robot'
 // 将样式以对象的方式加载进来，避免全局污染
 import style from './App.module.css'
+import logo from './assets/images/logo.svg'
 
 // const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image' />"
 // const jsHacked = "javascript: alert('Hacked');"
 function App() {
   return (
-    // react-jsx的规则，需要加<></>进行包裹
+    // react-jsx的规则，如果两个标签同级，需要加<></>进行包裹
     <div className={style.app}>
+      <div className={style.appHeader}>
+        <img className={style.appLogo} src={logo} alt="" />
+        <h1>罗伯特牛皮机器人商店</h1>
+      </div>
       {/* <a href={jsHacked}>123</a> */}
       <ul className={style.robotList}>
         {robots.map(r => <Robot id={r.id} name={r.name} email={r.email} />)}
