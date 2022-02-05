9f287515c826b58a46d62071acfdad97f46d1d86
liuyanbao <liuyanbao@shuidi-inc.com>
2022-02-05 12:11:03 +0800

    feat: 更新代码

---
+++ react-travel/src/index.tsx
@@ -0,0 +1,18 @@
+/*
+ * @Name: 
+ * @Description: 
+ * @Author: 刘燕保
+ * @Date: 2022-02-04 02:18:57
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
